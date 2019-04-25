f645ae5c2c79c9dc981748e417b5b9c4bb70b6c2
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-04-25 14:18:54 +0100

    reload command added

--- commands/win-7-cursor-pack.js
+++ commands/win-7-cursor-pack.js
@@ -2,21 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-      var colors = [
-          
-        "#345344",
-        "#345678",
-        "#598756",
-        "#987456",
-        "#875648",
-        "#456788",
-                    
-      ]
+
     let aEmbed = new Discord.RichEmbed()
   
     .setTitle(`Here is a link to the default cursor pack (Windows 7 | GitHub)`)
     .setURL("https://github.com/asadhum2005/win7-cursor-pack")
-    .setColor(colors);
+    .setColor("RANDOM");
   
     message.channel.send({embed: aEmbed});
   
